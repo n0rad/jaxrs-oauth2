47ac16c40dbd15f9fd77b4bee579835c8365848b
Brice Dutheil <brice.dutheil@gmail.com>
2014-01-16 13:59:25 +0100

    Changes the annotation license to use the one of spring as this is a derivative work from Spring's AnnotationUtils

--- api/src/main/java/fr/norad/jaxrs/oauth2/MetaAnnotationUtils.java
+++ api/src/main/java/fr/norad/jaxrs/oauth2/MetaAnnotationUtils.java
@@ -1,18 +1,17 @@
-/**
+/*
+ * Copyright 2002-2013 the original author or authors.
  *
- *     Copyright (C) norad.fr
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- *     Licensed under the Apache License, Version 2.0 (the "License");
- *     you may not use this file except in compliance with the License.
- *     You may obtain a copy of the License at
+ *      http://www.apache.org/licenses/LICENSE-2.0
  *
- *             http://www.apache.org/licenses/LICENSE-2.0
- *
- *     Unless required by applicable law or agreed to in writing, software
- *     distributed under the License is distributed on an "AS IS" BASIS,
- *     WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- *     See the License for the specific language governing permissions and
- *     limitations under the License.
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package fr.norad.jaxrs.oauth2;
 
@@ -22,6 +21,23 @@ import java.lang.reflect.Method;
 import java.util.Map;
 import java.util.WeakHashMap;
 
+/**
+ * General utility methods for working with meta-annotations, handling bridge methods (which the compiler
+ * generates for generic declarations) as well as super methods (for optional &quot;annotation inheritance&quot;).
+ * Note that none of this is provided by the JDK's introspection facilities themselves.
+ *
+ * @author Rob Harrop
+ * @author Juergen Hoeller
+ * @author Sam Brannen
+ * @author Mark Fisher
+ * @author Chris Beams
+ * @author Fabien Thouny
+ *
+ * @since 2.0
+ *
+ * @see java.lang.reflect.Method#getAnnotations()
+ * @see java.lang.reflect.Method#getAnnotation(Class)
+ */
 abstract class MetaAnnotationUtils {
 
     private static final Map<Class<?>, Boolean> annotatedInterfaceCache = new WeakHashMap<Class<?>, Boolean>();
